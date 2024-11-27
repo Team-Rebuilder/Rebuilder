ee9ac375f85a1642efc406f4cb152af53211221d
Daniel (Juyeong) Kim <kjy11gold@gmail.com>
2024-11-27 11:46:20 -0500

    Create necessary components

---
+++ rebuilder/src/app/components/three-d/three-d.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-three-d',
+  standalone: true,
+  imports: [],
+  templateUrl: './three-d.component.html',
+  styleUrl: './three-d.component.css'
+})
+export class ThreeDComponent {
+
+}
