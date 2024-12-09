1d2d64435bab62c54328b2f6b37593b61d00a1e6
Geesing <chewgeesing@gmail.com>
2024-12-09 17:21:51 -0500

    Create part-list component

---
+++ rebuilder/src/app/components/part-list/part-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-part-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './part-list.component.html',
+  styleUrl: './part-list.component.css'
+})
+export class PartListComponent {
+
+}
