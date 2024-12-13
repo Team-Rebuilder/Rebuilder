ec09b147aa23c6b41ee373680d2ce02d8dc165dc
Daniel (Juyeong) Kim <kjy11gold@gmail.com>
2024-12-13 12:18:00 -0500

    Responsive navbar

--- rebuilder/src/app/components/homenavbar/home.component.ts
+++ rebuilder/src/app/components/homenavbar/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { TabMenuModule } from 'primeng/tabmenu';
@@ -21,14 +21,33 @@ export class HomeComponent {
   items: MenuItem[] | undefined;
   activeItem: MenuItem | undefined;
   modelsService = inject(ModelsService);
+  private getScreenWidth: number;
 
   // Adapted from PrimeNG template
   constructor() {
-    this.items = [
+    this.getScreenWidth = window.innerWidth;
+    this.updateMenuItems();
+  }
+
+  // Refactored with the help of Copilot
+  private updateMenuItems(): void {
+    const isWideScreen = this.getScreenWidth > 600;
+    this.items = isWideScreen ? [
       { label: 'MODELS', icon: 'pi pi-th-large', routerLink: ['/models'] },
       { label: 'SUBMIT', icon: 'pi pi-cloud-upload', routerLink: ['/submit'] }
+    ] : [
+      { icon: 'pi pi-th-large', routerLink: ['/models'] },
+      { icon: 'pi pi-cloud-upload', routerLink: ['/submit'] }
     ];
     this.activeItem = this.items[0];
+  }
+
+  // Get screen width and height
+  // https://dev.to/dhanush9952/angular-13-detect-width-and-height-of-screen-tutorial-1of9
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+    this.updateMenuItems();
   }
 
   onActiveItemChange(event: MenuItem) {
--- rebuilder/src/app/components/modelnavbar/modelnavbar.component.ts
+++ rebuilder/src/app/components/modelnavbar/modelnavbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, inject } from '@angular/core';
+import { Component, input, inject, HostListener } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TabMenuModule } from 'primeng/tabmenu';
 import { MenuItem } from 'primeng/api';
@@ -16,12 +16,51 @@ import { ModelsService } from '../../services/models.service';
 export class ModelnavbarComponent {
   items: MenuItem[] | undefined;
   activeItem: MenuItem | undefined;
-  has3DModel = input<boolean>();
   modelsService = inject(ModelsService);
   id: string | undefined;
   currModel$: any;
+  private getScreenWidth: number;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute) {
+    this.getScreenWidth = window.innerWidth;
+    this.updateMenuItems();
+  }
+
+  // Refactored with the help of Copilot
+  private updateMenuItems(has3DModel: boolean = false): void {
+    const isWideScreen = this.getScreenWidth > 600;
+    const baseItems = [
+      {
+        label: isWideScreen ? 'Details' : undefined,
+        icon: 'pi pi-bars',
+        routerLink: ['../details']
+      },
+      {
+        label: isWideScreen ? 'Instructions' : undefined,
+        icon: 'pi pi-info-circle',
+        routerLink: ['../instructions']
+      }
+    ];
+
+    if (has3DModel) {
+      const threeDItem = {
+        label: isWideScreen ? '3D' : undefined,
+        icon: 'pi pi-box',
+        routerLink: ['../three']
+      };
+      baseItems.push(threeDItem);
+    }
+
+    this.items = baseItems;
+  }
+
+  // Get screen width and height
+  // https://dev.to/dhanush9952/angular-13-detect-width-and-height-of-screen-tutorial-1of9
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+    this.updateMenuItems(this.currModel$?.threemodelUrls?.length > 0);
+  }
 
   onActiveItemChange(event: MenuItem) {
     this.activeItem = event;
@@ -34,22 +73,10 @@ export class ModelnavbarComponent {
       this.id = params.get('id')!;
     });
 
-    // Get the model by id
-    // Since getModelById returns a promise, we need to await it
     this.currModel$ = await this.modelsService.getModelById(this.id!);
-
-    // Conditionally display the 3D model tab if 3D model present
-    this.items = this.currModel$.threemodelUrls.length > 0 ?
-    [
-      { label: 'Details', icon: 'pi pi-bars', routerLink: ['../details'] },
-      { label: '3D', icon: 'pi pi-box', routerLink: ['../three'] },
-      { label: 'Instructions', icon: 'pi pi-info-circle', routerLink: ['../instructions'] }
-    ] :
-    [
-      { label: 'Details', icon: 'pi pi-bars', routerLink: ['../details'] },
-      { label: 'Instructions', icon: 'pi pi-info-circle', routerLink: ['../instructions'] }
-    ];
-
-    this.activeItem = this.items[0];
+    this.updateMenuItems(this.currModel$.threemodelUrls.length > 0);
+    if (this.items && this.items.length > 0) {
+      this.activeItem = this.items[0];
+    }
   }
 }
